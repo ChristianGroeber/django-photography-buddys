431abe4a72b3b24563be22addf1eff3e9ea3e958
christiangroeber <christian.groeber@bluewin.ch>
2019-06-15 21:15:16 +0200

    added apps and static files

--- photography_buddys/photography_buddys/settings.py
+++ photography_buddys/photography_buddys/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'website',
+    'community',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -118,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static_cdn'
